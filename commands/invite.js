d97aeb4620536476e464abb719792706dcb70fae
AircraftOverviewer <maltesers42@gmail.com>
2021-02-07 00:32:02 +1000

    v2.1.1 (Edited)
    + Put "invite" command into an embed

--- commands/invite.js
+++ commands/invite.js
@@ -3,6 +3,10 @@ module.exports = {
   description: 'Sends the URL to invite this bot to any of your servers (must be admin for full capabilitis)',
   usage: '!invite',
   execute (Discord, message, args, client) {
-    message.channel.send('https://discord.com/api/oauth2/authorize?client_id=747720482668675112&permissions=8&scope=bot');
+    const invite = 'https://discord.com/api/oauth2/authorize?client_id=747720482668675112&permissions=8&scope=bot';
+    const inviteEmbed = new Discord.MessageEmbed()
+      .setColor()
+      .setDescription(`To invite the bot to your own server, click on [this link](${invite}) to be taken to Discord's verification page`);
+    message.channel.send(inviteEmbed);
   }
 };
