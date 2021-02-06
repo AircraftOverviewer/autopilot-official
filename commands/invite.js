19fecc6989b55567cca55ee4df48f634d186848d
AircraftOverviewer <maltesers42@gmail.com>
2021-02-07 00:36:33 +1000

    v2.1.1 (Edited)
    + Added HexColour to "invite"

--- commands/invite.js
+++ commands/invite.js
@@ -5,8 +5,8 @@ module.exports = {
   execute (Discord, message, args, client) {
     const invite = 'https://discord.com/api/oauth2/authorize?client_id=747720482668675112&permissions=8&scope=bot';
     const inviteEmbed = new Discord.MessageEmbed()
-      .setColor()
-      .setDescription(`To invite the bot to your own server, click on [this link](${invite}) to be taken to Discord's verification page`);
+      .setColor(message.guild.me.displayHexColor)
+      .setDescription(`To invite the bot to your own server, click on [this link](${invite}) to be taken to Discord's bot verification page`);
     message.channel.send(inviteEmbed);
   }
 };
