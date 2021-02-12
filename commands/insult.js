4dc1bc0e94a16bdf3765e71a37f04b8127da398a
AircraftOverviewer <maltesers42@gmail.com>
2021-02-12 12:09:30 +1000

    v2.2.1
    + Added 'insult' command. Allows users to have the bot insult another user. See '!help insult' for more information

---
+++ commands/insult.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: 'insult',
+  description: 'Have the bot insult a user of your choice',
+  usage: '!insult [user]',
+  parameters: {
+    1: { name: '[user]', description: '- Enter the tag of the user you want to insult' }
+  },
+  execute (Discord, message, args, client) {
+    const insulter = require('insult');
+    const taggedUser = message.mentions.users.first();
+    if (args[0]) {
+      message.channel.send(`${taggedUser} ${insulter.Insult()}`);
+    } else {
+      const tagEmbed = new Discord.MessageEmbed()
+        .setColor('#ff0000')
+        .setDescription('<:error:784747315960479754> You must tag the user you with to insult');
+      message.channel.send(tagEmbed);
+    }
+  }
+};
