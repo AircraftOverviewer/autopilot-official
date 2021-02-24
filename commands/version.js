787d23b9292c93ffbaa39531cebb3c2f17d3c622
AircraftOverviewer <maltesers42@gmail.com>
2021-02-24 17:04:49 +1000

    v2.3.3
    + Added 'weight' command

--- commands/version.js
+++ commands/version.js
@@ -6,8 +6,8 @@ module.exports = {
   execute (Discord, message, args, client) {
     const versionEmbed = new Discord.MessageEmbed()
       .setColor(message.guild.me.displayHexColor)
-      .setTitle('Current Version - v2.3.2')
-      .setDescription('```\n+ Added \'suvat\' command. Can be used to solve any unknowns from a set of known SUVATs. Try \'!help suvat\' for more information\n+ Added \'force\' command. Can be used to solve simple force equations using Newton\'s Second Law of Motion. Try \'!help force\' for more information\n+ Added \'ohms\' command. Can be used to solve Voltage, Current & Resistance in an electrical system\n+ Bug fixes```');
+      .setTitle('Current Version - v2.3.3')
+      .setDescription('```\n+ Added \'suvat\' command. Can be used to solve any unknowns from a set of known SUVATs. Try \'!help suvat\' for more information\n+ Added \'force\' command. Can be used to solve simple force equations using Newton\'s Second Law of Motion. Try \'!help force\' for more information\n+ Added \'ohms\' command. Can be used to solve Voltage, Current & Resistance in an electrical system\n+ Added \'weight\' command. Check how objects on Earth would weigh on differnet planets. Try \'!help weight\' for more information\n+ Bug fixes```');
     message.channel.send(versionEmbed);
   }
 };
