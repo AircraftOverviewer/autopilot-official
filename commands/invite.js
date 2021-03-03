04a37d3aad35361d0bfb7d3f6b5e4d52c198191b
AircraftOverviewer <maltesers42@gmail.com>
2021-03-03 16:56:24 +1000

    v2.4.0 (Edited)
    + Bug fixes

--- commands/invite.js
+++ commands/invite.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'invite',
-  description: 'Sends the URL to invite this bot to any of your servers (must be admin for full capabilitis)',
+  description: 'Sends the URL to invite this bot to any of your servers (must be admin for full capabilities)',
   usage: '!invite',
   execute (Discord, message, args, client) {
     const invite = 'https://discord.com/api/oauth2/authorize?client_id=747720482668675112&permissions=8&scope=bot';
