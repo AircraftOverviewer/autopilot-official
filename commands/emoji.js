93ff8a5a31630e2882f3393d5cb0e8a19c1323de
AircraftOverviewer <maltesers42@gmail.com>
2021-03-16 14:31:57 +1000

    v2.4.1
    + Changed 'emoji' & 'react' so that they can only send emoji's from a given server, and can only send 1 emoji at a time
    + Bug fixes

--- commands/emoji.js
+++ commands/emoji.js
@@ -1,28 +1,30 @@
 module.exports = {
   name: 'emoji',
-  description: 'Send any emoji within a given server, including animated emojis',
+  description: 'Send any emoji within this server, including animated emojis',
   usage: '!emoji [emoji_name]',
   parameters: {
     1: { name: '[emoji_name]', description: '- The full name of the emoji you wish to send. Does not need to be case-sensitive' }
   },
-  execute (Discord, message, args, client) {
-    const emojis = client.emojis.cache.filter(e => e.name.toLowerCase() === args.join(' ').toLowerCase());
+  async execute (Discord, message, args, client) {
+    if (!args[0]) {
+      const arg = new Discord.MessageEmbed()
+      .setColor('#ff0000')
+      .setDescription('<:error:784747315960479754> You must include an emoji to send')
+    message.channel.send(arg)
+    return;
+    }
 
-    let content = '';
+    let emoji;
 
-    emojis.forEach(emoji => {
-      content = `${content} ${emoji}`;
-    });
+    emoji = message.guild.emojis.cache.find(emoji => emoji.name.toLowerCase() === args[0].toLowerCase());
+    if (!emoji) emoji = message.guild.emojis.cache.find(emoji => emoji.name.toLowerCase().startsWith(args[0].toLowerCase()));
 
-    message.channel.send(content)
-      .then(function () {
-        message.delete();
-      })
-      .catch(function () {
-        const errorEmbed = new Discord.MessageEmbed()
-          .setColor('#ff0000')
-          .setDescription('<:error:784747315960479754> There was an error finding that emoji');
-        message.channel.send(errorEmbed);
-      });
+    const error = new Discord.MessageEmbed()
+      .setColor('#ff0000')
+      .setDescription('<:error:784747315960479754> Unable to locate that emoji')
+    if (!emoji) return message.channel.send(error);
+
+    await message.delete();
+    message.channel.send(emoji.toString());
   }
 };
--- commands/react.js
+++ commands/react.js
@@ -1,28 +1,28 @@
 module.exports = {
   name: 'react',
-  description: 'React to the latest message in a channel with any emoji within a given server, including animated emojis',
+  description: 'React to the latest message in a channel with any emoji in this server, including animated emojis',
   usage: '!react [emoji_name]',
   parameters: {
     1: { name: '[emoji_name]', description: '- The full name of the emoji you wish to react with. Does not need to be case-sensitive' }
   },
   async execute (Discord, message, args, client) {
     if (!args[0]) {
-      const emojiEmbed = new Discord.MessageEmbed()
-        .setColor('#ff0000')
-        .setDescription('<:error:784747315960479754> You must include an emoji to react with');
-      message.channel.send(emojiEmbed);
-      return;
+      const arg = new Discord.MessageEmbed()
+      .setColor('#ff0000')
+      .setDescription('<:error:784747315960479754> You must include an emoji to react with')
+    message.channel.send(arg)
+    return;
     }
 
-    const emoji = client.emojis.cache.filter(e => e.name.toLowerCase() === args[0].toLowerCase()).first();
+    let emoji;
 
-    if (!emoji) {
-      const nullEmbed = new Discord.MessageEmbed()
-        .setColor('#ff0000')
-        .setDescription('<:error:784747315960479754> There was an error finding that emoji');
-      message.channel.send(nullEmbed);
-      return;
-    }
+    emoji = message.guild.emojis.cache.find(emoji => emoji.name.toLowerCase() === args[0].toLowerCase());
+    if (!emoji) emoji = message.guild.emojis.cache.find(emoji => emoji.name.toLowerCase().startsWith(args[0].toLowerCase()));
+
+    const error = new Discord.MessageEmbed()
+      .setColor('#ff0000')
+      .setDescription('<:error:784747315960479754> Unable to locate that emoji')
+    if (!emoji) return message.channel.send(error);
 
     const result = await message.channel.messages.fetch({ limit: 2 });
 
